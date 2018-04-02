5c7c87ddb79f526bffbe855c7ebd179142156906
Demyanov Vyacheslav <demyanovs@gmail.com>
2018-04-02 14:58:38 +0300

    js

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,25 +11,25 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController extends AbstractController
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('page render');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
-
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
             'Woohoo! I\'m going on an all-asteroid diet!',
@@ -39,7 +39,15 @@ class ArticleController extends AbstractController
         return $this->render('article/show.html.twig',
             [
                 'title' => ucwords(str_replace('-', ' ', $slug)),
+                'slug' => $slug,
                 'comments' => $comments,
             ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart() {
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
